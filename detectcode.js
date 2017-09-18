e627e5c0efee1918160b04ad82a7ead243b2b74e
geoiraq <george.ejaam@gmail.com>
2017-09-18 13:20:39 +1000

    implemented code detection module - with usage

--- detectcode.js
+++ detectcode.js
@@ -1,6 +1,11 @@
 exports.detectCode = function(body) {
 //let us says we have an html with possible code blocks passed to here with html text content
 //we need to return an array of code blocks only
+// usage require the module first eg:
+// var dc = require('./detectcode');
+//send the html of your question or answer directly as is such as and invoke detectCode method:
+// var result = dc.detectCode(body);
+// returns [] empty array if no code detected or an array of codes blocks
 
 const cheerio = require('cheerio');
 const loadedCodes = [];
